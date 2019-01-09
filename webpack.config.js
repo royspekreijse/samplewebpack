bdb9b9a026f1f6051e0edeb9b98c81e5ee95c71b
Roy Spekreijse <roy@spekreijse.eu>
2019-01-09 22:42:06 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  }
+}
